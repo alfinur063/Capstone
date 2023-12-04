dc90a6da80909a01378cb8a51e979f92e0cd40ab
adit <adityawarman340@gmail.com>
2023-12-04 19:12:41 +0700

    add register activity, still had a problem with textInputEditText, need another design and logic in the code

--- app/src/main/java/com/example/capstoneproject/view/register/register.kt
+++ app/src/main/java/com/example/capstoneproject/view/register/register.kt
@@ -1,12 +1,40 @@
 package com.example.capstoneproject.view.register
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.capstoneproject.MainActivity
 import com.example.capstoneproject.R
+import com.example.capstoneproject.databinding.ActivityRegisterBinding
 
 class register : AppCompatActivity() {
+    private lateinit var binding: ActivityRegisterBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        setContentView(binding.root)
+
+        //regis to login page
+        binding.goToLogin.setOnClickListener{
+            startActivity(Intent(this@register, MainActivity::class.java))
+        }
+
     }
+
+    //create binding to connect register layout to login layout
+
+    //this 2 must have to finish tomorrow
+
+    //function to create account
+    private fun setUpAction(){
+        val username = null
+        val email = null
+        val password = null
+
+
+
+
+
+    }
+
 }
