d5248ee91f4e96a3fb2238ee4ffb5d7ca8044c19
alfinur063 <nurinayati1011@gmail.com>
2023-12-06 16:16:02 +0700

    noooob

--- app/src/main/java/com/example/capstoneproject/database/repository/Repository.kt
+++ app/src/main/java/com/example/capstoneproject/database/repository/Repository.kt
@@ -2,18 +2,42 @@ package com.example.capstoneproject.database.repository
 
 import android.app.Service
 import androidx.lifecycle.liveData
+import com.example.capstoneproject.database.api.ApiService
+import com.example.capstoneproject.database.preference.UserModel
+import com.example.capstoneproject.database.preference.UserPreference
+import com.example.capstoneproject.database.response.LoginResponse
+import com.example.capstoneproject.database.response.RegisterResponse
+import com.google.gson.Gson
+import retrofit2.HttpException
 import java.util.prefs.Preferences
 
 
 class Repository private constructor(
-    private val apiService: Service, //must be ApiService
-    private val preferences: Preferences //must be UserPreference
-){
-    fun login(email: String, password: String) = liveData<> {
+    private val apiService: ApiService,
+    private val preferences: UserPreference
+) {
+    fun login(email: String, password: String) = liveData<ResultState<LoginResponse>> {
         emit(ResultState.Loading)
         try {
-            val successResponse = apiService
-            val userModel = UserModel(successResponse.login)
+            val successResponse = apiService.login(email, password)
+            val userModel = UserModel(successResponse.loginResult.name, successResponse.loginResult.token, true)
+            emit(ResultState.Success(successResponse))
+        } catch (e: HttpException) {
+            val errorBody = e.response()?.errorBody()?.string()
+            val errorResponse = Gson().fromJson(errorBody, LoginResponse::class.java)
+            emit(ResultState.Error(errorResponse.message))
+        }
+    }
+
+    fun register(name: String, email: String, password: String) = liveData<ResultState<RegisterResponse>>{
+        emit(ResultState.Loading)
+        try {
+            val successResponse = apiService.register(name, email, password)
+            emit(ResultState.Success(successResponse))
+        } catch (e: HttpException) {
+            val errorBody = e.response()?.errorBody()?.string()
+            val errorResponse = Gson().fromJson(errorBody, RegisterResponse::class.java)
+            emit(ResultState.Error(errorResponse.message))
         }
     }
 }
