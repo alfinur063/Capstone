671997709e30135c06396bd32df71f00f697ef21
alfinur063 <nurinayati1011@gmail.com>
2023-12-08 00:25:09 +0700

    SearchActivity

---
+++ app/src/main/java/com/example/capstoneproject/view/search/SearchActivity.kt
@@ -0,0 +1,12 @@
+package com.example.capstoneproject.view.search
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.capstoneproject.R
+
+class SearchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search)
+    }
+}
