c1504b116dbd5f30cdf2195b2a5b04026e2a3aa8
adityawarman01 <142098760+adityawarman01@users.noreply.github.com>
2023-12-14 17:23:37 +0700

    Question layout is dones, have a nice day

---
+++ app/src/main/java/com/example/capstoneproject/view/question/QuestionActivity.kt
@@ -0,0 +1,12 @@
+package com.example.capstoneproject.view.question
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.capstoneproject.R
+
+class QuestionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_question)
+    }
+}
--- app/src/main/java/com/example/capstoneproject/view/termandcondition/TermsAndConditionActivity.kt
+++ app/src/main/java/com/example/capstoneproject/view/termandcondition/TermsAndConditionActivity.kt
@@ -6,16 +6,22 @@ import android.os.Bundle
 import android.widget.CheckBox
 import com.example.capstoneproject.MainActivity
 import com.example.capstoneproject.R
+import com.example.capstoneproject.databinding.ActivityTermsAndConditionBinding
+import com.example.capstoneproject.view.question.QuestionActivity
 
 class TermsAndConditionActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityTermsAndConditionBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_terms_and_condition)
+        binding = ActivityTermsAndConditionBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-
-        val checkbox : CheckBox = findViewById(R.id.checkbox)
-        when(checkbox) {
-
+        binding.goToQuestion.setOnClickListener {
+            startActivity(Intent(this@TermsAndConditionActivity, QuestionActivity::class.java))
         }
+
+
+
+
     }
 }
